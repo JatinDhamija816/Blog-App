5b82c88653cdc72473731a237c50edf5af994889
jdhamija816 <jdhamija816@gmail.com>
2024-12-30 20:58:01 +0530

    api response utils functions and app middleware cors & cookie-parser

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,15 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}`
-    );
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(
-      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+      `MongoDB connected: Database: ${connectionInstance.connection.name}, Host: ${connectionInstance.connection.host}`
     );
   } catch (error) {
-    console.error(`MongoDB connection Error `, error);
+    console.error("MongoDB connection error:", error.message);
+
     process.exit(1);
   }
 };
