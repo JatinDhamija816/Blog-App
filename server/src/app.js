7d5291aad7dc9f0c6c3755637904879c52420e51
jdhamija816 <jdhamija816@gmail.com>
2025-01-06 17:54:28 +0530

    register user

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -23,5 +24,7 @@ app.use(
 );
 app.use(express.static("public"));
 app.use(cookieParser);
+
+app.use("/api/v1/users", userRouter);
 
 export default app;
