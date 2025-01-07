2f1a0797bb3fbace5c22767dc861264f723956e2
jdhamija816 <jdhamija816@gmail.com>
2025-01-07 13:47:10 +0530

    navbar UI and home page setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Welcome from "./components/Welcome";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import ProfileSetup from "./components/ProfileSetup";
+import Home from "./components/Home";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile-setup" element={<ProfileSetup />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
